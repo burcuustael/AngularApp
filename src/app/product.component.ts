29b3abb3b1c5ddd258ff7404ccd72e1cbb1e443b
burcuustael <burcustael@gmail.com>
2024-08-06 16:23:48 +0300

    validation summary messages

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductRepository } from './repository.model';
 import { Product } from './product.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -20,5 +21,56 @@ export class ProductComponent {
     console.log('New Prodcut: ' + this.jsonProduct);
   }
 
- 
+  log(x: any) {
+    console.log(x);
+  }
+
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    console.log(form);
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
+
+  getFormValidationErrors(form: NgForm): string[] {
+    let messages: string[] = [];
+
+    Object.keys(form.controls).forEach((k) => {
+      console.log(k);
+      console.log(form.controls[k]);
+      this.getValidationErrors(form.controls[k], k).forEach((message) =>
+        messages.push(message)
+      );
+    });
+
+    return messages;
+  }
+
+  getValidationErrors(state: any, key: string) {
+    let ctrlName: string = state.name || key;
+    let messages: string[] = [];
+
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case 'required':
+            messages.push(`You must enter a ${ctrlName}`);
+            break;
+          case 'minlength':
+            messages.push(`Min. 3 characters required for ${ctrlName}`);
+            break;
+          case 'pattern':
+            messages.push(`The ${ctrlName} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
 }
